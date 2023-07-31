57d486772c16b8105bd5b3fae4eea059ce398f86
Anuj Johri <anujjohri7@gmail.com>
2023-07-31 16:44:30 -0400

    created a db table with default rows

--- first_n_fib_nums/fib_webapp/models.py
+++ first_n_fib_nums/fib_webapp/models.py
@@ -7,5 +7,14 @@ class FibonacciTable(models.Model):
     fib_no = models.BigIntegerField();
     value = models.BigIntegerField();
 
+    @classmethod
+    def add_default_rows(cls):
+        rows = [
+            FibonacciTable(fib_no=1, value=0),
+            FibonacciTable(fib_no=2, value=1)
+        ]
+
+        FibonacciTable.objects.bulk_create(rows)
+
     def __str__(self):
         return f'{fib_no} fibonaccti number is {value}.'
--- first_n_fib_nums/fib_webapp/views.py
+++ first_n_fib_nums/fib_webapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+from .models import FibonacciTable
 
 # Create your views here.
+
+@require_http_methods(['GET'])
+def fib_nums(request):
+    num = request.GET.get('num')
+
+    #checking if there is an entry in the db already for num
+    db = FibonacciTable.objects.filter(fib_no__lte=num)
+
+    if db:
+        # return directly
+        return 0
+    
+    else:
+        #calculate the remaining fibnums
+        pass

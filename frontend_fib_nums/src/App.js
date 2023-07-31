4f6919c506fccde5064883cb899e33d90a6a297f
Anuj Johri <anujjohri7@gmail.com>
2023-07-31 02:26:34 -0400

    Working on first page form

--- frontend_fib_nums/src/App.js
+++ frontend_fib_nums/src/App.js
@@ -2,10 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div style={{display: 'flex', flexDirection: 'column'}}>
     <h1 style={{margin: '5%', alignSelf: 'center'}}>Fibonacci Numbers Generator</h1>
     <h2 style={{margin: '5%', alignSelf: 'center'}}>Enter a positive integer(n) for generating first n fibonacci numbers</h2>
+    <input type="number" value={1}/>
+    <button>Submit</button>
     </div>
   );
 }
